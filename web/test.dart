203a8a00cb028ebe1c65b32358da00a65150d879
James deBoer <james@huronbox.com>
2015-06-05 19:51:14 -0700

    Test case for router stack traces

--- web/test.dart
+++ web/test.dart
@@ -2,26 +2,21 @@ import 'dart:html';
 import 'dart:async';
 
 import 'package:angular2/angular2.dart';
+import 'package:angular2/router.dart';
 import 'package:angular2/di.dart';
 import 'package:angular2/src/render/dom/shadow_dom/shadow_dom_strategy.dart';
 import 'package:angular2/src/render/dom/shadow_dom/native_shadow_dom_strategy.dart';
 import 'package:angular2/src/reflection/reflection.dart';
 import 'package:angular2/src/reflection/reflection_capabilities.dart';
 
-origin() {
-  throw 'An exception. Can you see origin() in the stack?';
-}
-
 amain() {
-  print('This program will trigger a stack trace. The function origin() \n'
-    'should be included in the stack trace.');
-
   // For now, use dynamic reflection (mirrors).
   // https://github.com/angular/angular/issues/1063
   reflector.reflectionCapabilities = new ReflectionCapabilities();
   final testBindings = [
     bind(ShadowDomStrategy).toClass(NativeShadowDomStrategy)
   ];
+  testBindings.addAll(routerInjectables);
 
   bootstrap(MyComp, testBindings);
 }
@@ -35,10 +30,23 @@ main() {
 }
 
 @Component(selector: 'my-comp')
+@View(
+  directives: const [RouterLink, RouterOutlet],
+  template: """<router-outlet name="default"></router-outlet>
+  <a router-link="error" href="#" target rel="noreferrer">Error for me</a>""")
+@RouteConfig(const [
+  const {
+    'path': '/error',
+    'component': DefaultView,
+    'as': 'error'
+  }])
 class MyComp {
-  MyComp() {
-    print('Calling origin');
-    origin();
-    print('Done origin');
+}
+
+@Component(selector: 'why-do-i-need-this')
+class DefaultView {
+  DefaultView() {
+    print('DefaultView created. I will now throw an exception');
+    throw 'Do you see it now?';
   }
 }
